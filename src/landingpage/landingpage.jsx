f45b4abb9f6ae78ca9c3ebed1df39d4ce05edb64
Jeremiah Nyok <98920854+a1bx@users.noreply.github.com>
2023-09-28 17:03:28 +0300

    Update landingpage.jsx

--- src/landingpage/landingpage.jsx
+++ src/landingpage/landingpage.jsx
@@ -5,7 +5,7 @@ function LandingPage (){
     <div className="bg-cover bg-center h-screen bg-image">
       <div className="flex items-center justify-center h-full">
         <div className="text-center">
-          <h1 className="text-4xl font-bold text-white mb-8">Welcome to votechain App make choices here</h1>
+          <h1 className="text-4xl font-bold text-white mb-8">Welcome to votechain make choices here</h1>
           <div className="space-x-4">
             <button className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded-full shadow-md">
               Login
